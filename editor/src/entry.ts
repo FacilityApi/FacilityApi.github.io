7965c17cc2748b98682faee03536c1cade64e376
Ed Ball <ed.ball@logos.com>
2016-11-20 09:33:37 -0800

    Use @types/requirejs.

--- editor/src/entry.ts
+++ editor/src/entry.ts
@@ -1,11 +1,5 @@
 import run from './index';
-
-declare var require: {
-    <T>(path: string): T;
-    (paths: string[], callback: (...modules: any[]) => void): void;
-    ensure: (paths: string[], callback: (require: <T>(path: string) => T) => void) => void;
-};
-require('es6-promise/auto');
+import 'es6-promise/auto';
 
 if ((window as any)['monaco']) {
 	run();
